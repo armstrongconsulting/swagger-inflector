c1ad60ca0be0909e3feaaca279cd1bbfd088789d
Christoph Schachinger <christoph@armstrongconsulting.net>
2017-05-11 16:23:28 +0200

    cors

--- src/main/java/io/swagger/inflector/utils/CORSFilter.java
+++ src/main/java/io/swagger/inflector/utils/CORSFilter.java
@@ -16,22 +16,36 @@
 
 package io.swagger.inflector.utils;
 
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class CORSFilter implements javax.servlet.Filter {
+ 
+    
+    private Set<String> allowHeaders = new HashSet<String>();
+    
+    public CORSFilter allowHeader(String name){
+    	allowHeaders.add(name);
+    	return this;
+    }
+   
+    public String allow_header;
+    
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
                          FilterChain chain) throws IOException, ServletException {
         HttpServletResponse res = (HttpServletResponse) response;
         res.addHeader("Access-Control-Allow-Origin", "*");
         res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-        res.addHeader("Access-Control-Allow-Headers", "Content-Type, api_key, Authorization");
+        res.addHeader("Access-Control-Allow-Headers", allow_header);
         chain.doFilter(request, response);
     }
 
@@ -41,5 +55,11 @@ public class CORSFilter implements javax.servlet.Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+    	allowHeaders.add("Content-Type");
+      	allowHeaders.add("api_key");
+      	allowHeaders.add("Authorization");
+      	
+      	allow_header = allowHeaders.stream().reduce((t, u) -> t + ", " + u).get();
+       
     }
 }
